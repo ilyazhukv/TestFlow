f2755e24a9b160c8c72219ea631a8a2e22cc6878
ilyazhukv <ilya.zhukv@gmail.com>
2026-02-13 13:40:07 +0600

    Admin: allow role updates via ID in request body

--- backend/controllers/admin.controller.js
+++ backend/controllers/admin.controller.js
@@ -2,14 +2,14 @@ import User from "../models/User.js";
 
 export const updateUserRole = async (req, res) => {
   try {
-    const { role } = req.body;
+    const { _id, role } = req.body;
     const validRole = ["user", "admin"];
     if (!validRole.includes(role)) return res.status(400).json({ message: "Invalid role" });
 
-    const updateUser = await User.findByIdAndUpdate(req.params.id, { role }, { new: true });
-    if (!updateUser) return res.status(404).json({ message: "User not found" });
+    const updatedUser = await User.findByIdAndUpdate( _id, { role }, { new: true } );
+    if (!updatedUser) return res.status(404).json({ message: "User not found" });
 
-    res.status(200).json(updateUser);
+    res.status(200).json(updatedUser);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
