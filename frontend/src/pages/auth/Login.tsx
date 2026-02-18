3a51a04bac367fe90641b3de3ee13801512809dd
ilyazhukv <ilya.zhukv@gmail.com>
2026-02-18 22:52:30 +0600

    Fixed styles for login and register

--- frontend/src/pages/auth/Login.tsx
+++ frontend/src/pages/auth/Login.tsx
@@ -1,12 +1,20 @@
-import { Container, Box, Typography, TextField, Button, Alert } from "@mui/material";
+import {
+  Container,
+  Box,
+  Typography,
+  TextField,
+  Button,
+  Alert,
+} from "@mui/material";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
 
 import api from "../../api/api";
+import axios from "axios";
 
 const Login = () => {
-  const [formData, setFormData] = useState({login: "", password: ""});
+  const [formData, setFormData] = useState({ login: "", password: "" });
   const [errorMsg, setErrorMsg] = useState<string | null>(null);
   const { login } = useAuth();
   const navigate = useNavigate();
@@ -17,43 +25,78 @@ const Login = () => {
 
     setFormData(() => ({
       ...formData,
-      [name]: value
-    }))
-  }
+      [name]: value,
+    }));
+  };
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    
+
     try {
       const resp = await api.post("/user/login", formData);
       const { token } = resp.data;
-      login(token)
-      navigate("/")
-    } catch (error: any) {
-      console.error("Error:", error);
-      const message = error.response?.data?.message || "Something went wrong";
-      setErrorMsg(message);
+      login(token);
+      navigate("/");
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        axios.isAxiosError(error);
+        console.error("Error:", error);
+        const message = error.response?.data?.message || "Something went wrong";
+        setErrorMsg(message);
+      }
     }
-  }
+  };
 
   return (
-    <Container maxWidth="xs">
+    <Container sx={{minHeight: "100vh", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column"}}>
       {errorMsg && (
-        <Alert severity="error" sx={{ width: '100%', mb: 2 }}>
+        <Alert severity="error" sx={{ width: "45%", mb: 2 }}>
           {errorMsg}
         </Alert>
       )}
-      <Box sx={{mt: 8, display: "flex", flexDirection: "column", alignItems: "center"}}>
-        <Typography variant="h5" component="h1">Sign-In</Typography>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h5" component="h1">
+          Sign-In
+        </Typography>
         <Typography component="p">Welcome</Typography>
         <Box component="form" onSubmit={handleSubmit}>
-          <TextField name="login" value={formData.login} onChange={handleChange} label="Name or email" fullWidth margin="normal" required />
-          <TextField name="password" value={formData.password} onChange={handleChange} label="Password" fullWidth margin="normal" type="password" required />
-          <Button type="submit" variant="contained" fullWidth sx={{mt: 3, mb: 2}}>Sign In</Button>
+          <TextField
+            name="login"
+            value={formData.login}
+            onChange={handleChange}
+            label="Login"
+            fullWidth
+            margin="normal"
+            required
+          />
+          <TextField
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            label="Password"
+            fullWidth
+            margin="normal"
+            type="password"
+            required
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            fullWidth
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
         </Box>
       </Box>
     </Container>
-  )
-}
+  );
+};
 
 export default Login;
--- frontend/src/pages/auth/Register.tsx
+++ frontend/src/pages/auth/Register.tsx
@@ -1,11 +1,23 @@
-import { Container, Box, Typography, TextField, Button, Alert } from "@mui/material";
+import {
+  Container,
+  Box,
+  Typography,
+  TextField,
+  Button,
+  Alert,
+} from "@mui/material";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import api from "../../api/api";
+import axios from "axios";
 
 const Register = () => {
-  const [formData, setFormData] = useState({name: "", email: "", password: ""});
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
   const [errorMsg, setErrorMsg] = useState<string | null>(null);
   const navigate = useNavigate();
 
@@ -15,42 +27,85 @@ const Register = () => {
 
     setFormData(() => ({
       ...formData,
-      [name]: value
+      [name]: value,
     }));
   };
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    
+
     try {
       await api.post("/user/register", formData);
-      navigate("/login")
-    } catch (error: any) {
-      console.error("Error:", error);
-      const message = error.response?.data?.messege || "Something went wrong";
-      setErrorMsg(message);
+      navigate("/login");
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.error("Error:", error);
+        const message = error.response?.data?.messege || "Something went wrong";
+        setErrorMsg(message);
+      }
     }
-  }
+  };
 
   return (
-    <Container maxWidth="xs">
+    <Container sx={{minHeight: "100vh", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column"}}>
       {errorMsg && (
-        <Alert severity="error" sx={{ width: '100%', mb: 2 }}>
+        <Alert severity="error" sx={{ width: "100%", mb: 2 }}>
           {errorMsg}
         </Alert>
       )}
-      <Box sx={{mt: 8, display: "flex", flexDirection: "column", alignItems: "center"}}>
-        <Typography variant="h5" component="h1">Sign-Up</Typography>
+      <Box
+        sx={{
+          mt: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h5" component="h1">
+          Sign-Up
+        </Typography>
         <Typography component="p">Welcome</Typography>
         <Box component="form" onSubmit={handleSubmit}>
-          <TextField name="email" value={formData.email} onChange={handleChange} label="Email" fullWidth margin="normal" required />
-          <TextField name="name" value={formData.name} onChange={handleChange} label="Name" fullWidth margin="normal" required />
-          <TextField name="password" value={formData.password} onChange={handleChange} label="Password" fullWidth margin="normal" type="password" required />
-          <Button type="submit" variant="contained" fullWidth sx={{mt: 3, mb: 2}}>Sign Up</Button>
+          <TextField
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            label="Email"
+            fullWidth
+            margin="normal"
+            required
+          />
+          <TextField
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            label="Name"
+            fullWidth
+            margin="normal"
+            required
+          />
+          <TextField
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            label="Password"
+            fullWidth
+            margin="normal"
+            type="password"
+            required
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            fullWidth
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign Up
+          </Button>
         </Box>
       </Box>
     </Container>
-  )
-}
+  );
+};
 
 export default Register;
