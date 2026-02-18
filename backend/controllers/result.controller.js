adbf7e56d028d39ae0778563ad6a12d572808707
ilyazhukv <ilya.zhukv@gmail.com>
2026-02-18 16:42:16 +0600

    Added: getResult, deleteResult

--- backend/controllers/result.controller.js
+++ backend/controllers/result.controller.js
@@ -1,9 +1,10 @@
 import Result from "../models/Result.js"
+import Test from "../models/Test.js";
 
-export const saveResult = async (res, req) => {
+export const saveResult = async (req, res) => {
   try {
     const { testId, userId, answers } = req.body;
-
+    const test = await Test.findById(testId).lean();
     if (!test) return res.status(404).json({ message: "Test not found" });
 
     let score = 0;
@@ -13,15 +14,21 @@ export const saveResult = async (res, req) => {
     test.questions.forEach(question => {
       maxScore += question.points;
 
-      const userAnswers = answers.find(a => String(a.questionId) === String(question._id));
+      const userAnswer = answers.find(a => String(a.questionId) === String(question._id));
+      if (!userAnswer) return;
 
-      const isCorrect = userAnswers ? question.options.find(opt => String(opt._id) === String(userAnswers.SelectedOptionId)) : false;
+      const correctOptionIds = question.options.filter(opt => opt.isCorrect).map(opt => String(opt._id));
+
+      const userSelectedIds = userAnswer?.selectedOptionIds.map(id => String(id));
+
+      const isCorrect = correctOptionIds.length === userSelectedIds.length &&
+        correctOptionIds.every(id => userSelectedIds.includes(id));
 
       if (isCorrect) score += question.points;
 
       processedAnswers.push({
         questionId: question._id,
-        SelectedOptionId: userAnswers?.SelectedOptionId,
+        selectedOptionIndexes: userSelectedIds,
         isCorrect
       });
     });
@@ -44,8 +51,30 @@ export const saveResult = async (res, req) => {
 
 export const getResult = async (req, res) => {
   try {
+    const userId = req.params.id;
+    const result = await Result.findById(userId);
+  } catch (error) {
+
+  }
+}
+
+export const getResults = async (req, res) => {
+  try {
     const results = await Result.find().sort({ completedAt: -1 });
     res.json(results);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
+export const deleteResult = async (req, res) => {
+  try {
+    const { resultId } = req.body;
+
+    const deleteResult = await Result.findByIdAndDelete(resultId);
+    if (!deleteResult) res.status(200).json({ message: "Result not found" });
+
+    res.status(200).json({ message: "The result has been deleted successfully" })
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
