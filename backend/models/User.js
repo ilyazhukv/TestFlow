7543ce065f941076f73b85147f041baa7049e76b
ilyazhukv <ilya.zhukv@gmail.com>
2026-02-19 14:32:48 +0600

    Modified user schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true, lowercase: true },
   email: { type: String, required: true, unique: true, lowercase: true, trim: true, match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ },
-  passwordHash: { type: String, required: true },
+  passwordHash: { type: String, required: true, select: false },
   role: { type: String, enum: ["user", "admin"], default: "user" },
   createdAt: { type: Date, default: Date.now }
 });
