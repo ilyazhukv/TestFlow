2aee87c19ae8d449329622f7e980c2680b3b29a0
ilyazhukv <ilya.zhukv@gmail.com>
2026-02-21 02:20:13 +0600

    Added validator

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,10 +2,11 @@ import express from "express";
 import { registerUser, loginUser, getUsers, deleteUser } from "../controllers/user.controller.js";
 import authMiddleware from "../middleware/auth.middleware.js";
 import requireRoles from "../middleware/role.middleware.js";
+import validator from "../middleware/validator.middleware.js";
 
 const router = express.Router();
 
-router.post("/register", registerUser);
+router.post("/register", validator({email: true, password: 4}), registerUser);
 router.post("/login", loginUser);
 router.get("/", authMiddleware, requireRoles(["admin"]), getUsers);
 router.delete("/delete", authMiddleware, deleteUser);
