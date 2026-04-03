80154c4f21f2159b2ff8211105ab1147411ebf4a
ilyazhukv <ilya.zhukv@gmail.com>
2026-04-03 20:51:12 +0600

    feat: make user uniqueness case-insensitive using collation

--- server/models/User.js
+++ server/models/User.js
@@ -2,12 +2,14 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   avatar: { type: String, trim: true },
-  name: { type: String, required: true, index: { unique: true, collation: { strength: 2 } } },
-  email: { type: String, required: true, index: { unique: true, collation: { strength: 2 } } },
+  name: { type: String, required: true, unique: true, trim: true },
+  email: { type: String, required: true, unique: true, trim: true },
   passwordHash: { type: String, required: true, select: false },
   role: { type: String, enum: ["user", "admin"], default: "user" },
   status: {type: String, enum: ["stopped", "active"], default: "active"},
   createdAt: { type: Date, default: Date.now }
+}, { 
+  collation: { locale: 'en', strength: 2 } 
 });
 
 userSchema.pre('findOneAndDelete', async function (next) {
